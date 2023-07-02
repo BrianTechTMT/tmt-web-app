3cec016a662158a9660d92d1b1c3886b34d4a41e
trietnguyen <stephaneoflord@gmail.com>
2023-07-02 01:37:18 -0700

    commit

---
+++ src/components/pages/ER.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap';
+function ER(){
+    return(
+        <>
+        <Container className='justify-content-md-center'>
+            <h2 align="center">Eucharistic Revival</h2>
+            <Row className='justify-content-md-center'>
+                <Col align="center">
+                    <h3> What is the meaning of the Eucharist? </h3>
+                    <p>The Eucharist is the source and summit of the Christian life. The term “Eucharist” originates from the Greek word eucharistia, meaning thanksgiving.</p>
+                    <Col align="center">
+                        <h3>“This is my body which is given for you. Do this in remembrance of me. (Lk 22:19)”</h3>
+                    </Col>
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
+export default ER;
