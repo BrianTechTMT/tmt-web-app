d8d534af91f61e7a762638877984e9dc22709733
trietnguyen <stephaneoflord@gmail.com>
2023-07-04 13:18:54 -0700

    commit

---
+++ src/components/pages/Gallery.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+function ImageGallery(){
+    return(
+        <>
+        <Container className="justify-content-md-center pt-3">
+            <h3> Hello World</h3>
+        </Container>
+        </>
+    )
+}
+export default ImageGallery;
