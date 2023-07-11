13d7e823d0ba755048db4b2d99013efa7b164668
trietnguyen <stephaneoflord@gmail.com>
2023-07-11 02:30:23 -0700

    hi

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from 'react';
 function ImageComponent() {
   const imageFiles = useState([]);
 
+  useEffect(() => {
+    fetch('/home/trior/TMTSite/tmt-web-app/src/components/images/TNImages/doan-tmt.org/doan-tmt.org/gallery/galleries/2022-2023/Binh_Minh_29/Set_A/images')  // replace with your server address
+      .then(response => response.json())
+      .then(data => setImageFiles(data));
+  }, []);
+
   return (
     <div>
       {imageFiles.map((file, index) => (
