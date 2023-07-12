110939fe3168edb40ba124b5c937977377e3c997
trietnguyen <stephaneoflord@gmail.com>
2023-07-12 11:30:26 -0700

    hi

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -20,14 +20,15 @@ class ImageComponent extends React.Component {
     return (
         <>
       <div className='pt-5'>
-
-
             <Container className='pt-5'>
-                <Row className='justify-content-md-center'>
+                <Row className='justify-content-md-center' align="center">
                     {imageFiles.map((file, index) => (
-                        <Card style={{ width: '18rem' }} className="d-flex" align="center">
-                            <Card.Img className= "justify-content-md-center" align="center" variant="top" src={file}  />
-                        </Card>
+                        <Col className='justify-content-sm-center pt-3' align="center">
+                            <Card style={{ width: '11.7rem', }} className="border rounded-5 shadow-lg p-3 mb-5 bg-body rounded " align="center">
+                                <h3 style={{fontSize:'10px'}}>Images {index+1}</h3>
+                                <Card.Img style ={{height: '9.7rem', width: '9.7rem'}} className= "d-plex justify-content-md-center border rounded" align="center" variant="top" src={file}/>
+                            </Card>
+                        </Col>
                     ))}
                 </Row>
             </Container>
